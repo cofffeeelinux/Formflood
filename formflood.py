c96f121201f46d68e38577bfb81ec3dca7a54103
cofffeeelinux <37485298+cofffeeelinux@users.noreply.github.com>
2020-09-10 10:51:03 -0700

    latest and greatest

--- formflood.py
+++ formflood.py
@@ -5,28 +5,43 @@ import string
 import json
 import time
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+# chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
 url = 'https://www.associate4you.com/'
 
+# passwordfield = 'field id'
 personfield = 'form_fields[name]'
 emailfield = 'form_fields[email]'
 messagefield = 'form_fields[message]'
 
-names = json.loads(open('names.json').read())
-lastname = json.loads(open('lastname.json').read())
+# calling all Json Files
+firstnames = json.loads(open('names.json').read())
+lastnames = json.loads(open('lastnames.json').read())
+emaildomains = json.loads(open('emaildomains.json').read())
+messages = json.loads(open('messages.json').read())
 
-for name in names:
-    name_extra = ''.join(random.choice(string.digits))
+print('Finding a unique person...')
 
-    email = random.choice(names).lower() + name_extra + "@gmail.com"
-    password = ''.join(random.choice(chars) for i in range(22))
-    message = "I love Bella is he still available"
-    person = name + ' ' + (random.choice(lastname))
+# for loop could be better, but it works for now...
+for name in range(1000):
+    # generating some variables to be used later
+    name_extra = ''.join(random.choice(string.digits) for i in range(4))
+    lastname = random.choice(lastnames)
+    firstname = random.choice(firstnames)
+    emaildomain = random.choice(emaildomains)
+    message = random.choice(messages)
+
+    email = firstname.lower() + lastname.lower() + name_extra + emaildomain
+    # password = ''.join(random.choice(chars) for i in range(22))
+    person = firstname + ' ' + lastname
+    time.sleep(5)
+    print(person + ' found!')
+
 
 # making the post request to be sent to the website, this needs to be updated PER WEBSITE
     try:
+
         requests.post(url, allow_redirects=False, data={
             'post_id': '',
             'form_id': '',
@@ -36,11 +51,21 @@ for name in names:
             messagefield: message,
             'action': 'elementor_pro_forms_send_form'
          })
+        print(person + ' is interested in buying a cat!')
+        time.sleep(5)
+        print('Request sent!')
+        time.sleep(5)
     except:
-        print('server is not responding; sleeping 200 seconds')
-        time.sleep(200)
+        print('He doesnt like cats, trying again')
+        time.sleep(60)
 
-    print('sending email %s, name %s and message %s' % (email, person, message))
+    # need to wait some time or the server thinks you are DOS'ing them
+    # added some stuff to read while we wait, added time to different prints
+    print(firstname + ('s email is: ' + email))
+    time.sleep(5)
+    print(firstname + (' said: ' + message))
+    time.sleep(5)
+    print('Looking for another person.')
+    time.sleep(5)
 
-    print('sleeping for 15 seconds')
-    time.sleep(15)
+print('Man these people love cats!')
